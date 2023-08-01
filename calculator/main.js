1c7c5c947180fa944c3397a495557208d1ddb5c7
ajimah kenechukwu <kenechukwuajimah@gmail.com>
2023-08-01 13:01:44 +0100

    calculator

---
+++ calculator/main.js
@@ -0,0 +1,30 @@
+(function(){
+
+    let screen =  document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn-num');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    buttons.forEach(function(button){
+        button.addEventListener('click',function(e){
+            let value = e.target.dataset.num;
+            screen.value += value;
+            console.log(screen.value );
+        })
+    });
+
+    equal.addEventListener('click', function(e){
+        if (screen.value === ''){
+            screen.value = "please enter";
+        }else{
+            let answer = eval(screen.value);
+            screen.value += " = " + answer;
+            console.log(screen.value );
+        }
+    })
+        clear.addEventListener('click', function(e){
+
+            screen.value = "" ;
+
+        })
+})();
